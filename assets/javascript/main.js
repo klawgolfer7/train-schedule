063945fe8f5e40b71b33acc04cfbfc4219e3ab32
Kevin Lawler <kevinrlawler@gmail.com>
2017-01-30 21:36:50 -0500

    adding jquery bootstrap and firebase

---
+++ assets/javascript/main.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+apiKey: "AIzaSyDnYokM_9lNNbbVLdsCoi5Z3VdPDP-SsDQ",
+authDomain: "train-schedule-7f06f.firebaseapp.com",
+databaseURL: "https://train-schedule-7f06f.firebaseio.com",
+storageBucket: "train-schedule-7f06f.appspot.com",
+messagingSenderId: "962062933098"
+};
+firebase.initializeApp(config);
+
